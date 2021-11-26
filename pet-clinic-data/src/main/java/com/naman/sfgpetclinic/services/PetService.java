327d9e1504ef686832c41f8a236e6ae9a5782596
Naman M Samarth <namansamarth1@gmail.com>
2021-11-26 22:29:45 +0530

    Adding Interfaces for services. closes #5

---
+++ pet-clinic-data/src/main/java/com/naman/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.naman.sfgpetclinic.services;
+
+import com.naman.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/naman/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.naman.sfgpetclinic.services;
+
+import com.naman.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
