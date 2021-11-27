c990f413970495e3e702c3cabc70debe3d35defa
Naman M Samarth <namansamarth1@gmail.com>
2021-11-27 23:27:18 +0530

    refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/com/naman/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/naman/sfgpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.naman.sfgpetclinic.services;
 
 import com.naman.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
