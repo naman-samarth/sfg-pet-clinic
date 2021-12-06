052ea3157f378c8cbf45a31aa0c57ffb624a292a
Naman M Samarth <namansamarth1@gmail.com>
2021-12-06 22:04:16 +0530

    added SpecialityService Map Impl, closes #31.

---
+++ pet-clinic-data/src/main/java/com/naman/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package com.naman.sfgpetclinic.services;
+
+import com.naman.sfgpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/naman/sfgpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,35 @@
+package com.naman.sfgpetclinic.services.map;
+
+import com.naman.sfgpetclinic.model.Speciality;
+import com.naman.sfgpetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
