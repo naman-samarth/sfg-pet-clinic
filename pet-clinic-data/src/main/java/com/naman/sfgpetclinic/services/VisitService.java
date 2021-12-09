7aa5aef5cf333100bd675135fc8a93ae68d7925f
Naman M Samarth <namansamarth1@gmail.com>
2021-12-10 00:21:48 +0530

    added map service for Visit, closes #46

---
+++ pet-clinic-data/src/main/java/com/naman/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.naman.sfgpetclinic.services;
+
+import com.naman.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/naman/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,40 @@
+package com.naman.sfgpetclinic.services.map;
+
+import com.naman.sfgpetclinic.model.Visit;
+import com.naman.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null ||
+                visit.getPet().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
