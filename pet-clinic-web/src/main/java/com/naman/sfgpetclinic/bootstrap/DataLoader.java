b5515fabe4c0f6ae9ffba72381e7c4238f457523
Naman M Samarth <namansamarth1@gmail.com>
2021-12-10 00:30:57 +0530

    added bootstrap data for Visits, closes #47

--- pet-clinic-web/src/main/java/com/naman/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/naman/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.naman.sfgpetclinic.bootstrap;
 
 import com.naman.sfgpetclinic.model.*;
-import com.naman.sfgpetclinic.services.OwnerService;
-import com.naman.sfgpetclinic.services.PetTypeService;
-import com.naman.sfgpetclinic.services.SpecialityService;
-import com.naman.sfgpetclinic.services.VetService;
+import com.naman.sfgpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,14 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -85,6 +84,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners....");
 
